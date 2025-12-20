1cbd9b5281b4b532ce1a6e28460249d481cb9f13
Anas <anasmony15@gmail.com>
2025-12-21 01:46:39 +0200

    Class Update and Deletion service and controlller functions completed

--- src/routes/class.routes.js
+++ src/routes/class.routes.js
@@ -1,37 +1,67 @@
 const express = require("express");
 const router = express.Router();
 
-const classController = require('../controllers/class.controller');
-const { authenticate, authorize, authorizeClassRole } = require('../middlewares/auth');
-const sectionRoutes = require('./section.routes');
+const classController = require("../controllers/class.controller");
+const {
+  authenticate,
+  authorize,
+  authorizeClassRole,
+} = require("../middlewares/auth");
+const sectionRoutes = require("./section.routes");
 // POST /api/classes
-router.post('/create', 
-    authenticate, 
-    authorize('Instructor'), 
-    classController.createClass);
+router.post(
+  "/create",
+  authenticate,
+  authorize("Instructor"),
+  classController.createClass
+);
 
-router.post('/:classId/invite', 
-    authenticate, 
-    authorize('Instructor'),
-    authorizeClassRole('admin'),
-    classController.inviteUser);
+router.post(
+  "/:classId/invite",
+  authenticate,
+  authorize("Instructor"),
+  authorizeClassRole("admin"),
+  classController.inviteUser
+);
 
 // GET /api/classes
-router.get('/:userId', 
-    authenticate, 
-    classController.getClasses);
-    
-router.get('/:classId/class-code', 
-    authenticate, 
-    authorize('Instructor'),
-    authorizeClassRole('admin'), 
-    classController.getClassCode);
+router.get("/:userId", authenticate, classController.getClasses);
+
+router.get(
+  "/:classId/class-code",
+  authenticate,
+  authorize("Instructor"),
+  authorizeClassRole("admin"),
+  classController.getClassCode
+);
+
+router.get(
+  "/:classId/class-code",
+  authenticate,
+  authorize("Instructor"),
+  authorizeClassRole("admin"),
+  classController.getClassCode
+);
+
+router.patch(
+  "/edit/:classId",
+  authenticate,
+  authorize("Instructor"),
+  authorizeClassRole("admin"),
+  classController.editClass
+);
+
+router.delete(
+  "/delete/:classId",
+  authenticate,
+  authorize("Instructor"),
+  authorizeClassRole("admin"),
+  classController.deleteClass
+);
 
 // /api/classes/:id/sections
-router.use('/', sectionRoutes);
+router.use("/", sectionRoutes);
 
-router.get('/:classId/search-users',
-    authenticate,
-    classController.searchUsers);
+router.get("/:classId/search-users", authenticate, classController.searchUsers);
 
 module.exports = router;
