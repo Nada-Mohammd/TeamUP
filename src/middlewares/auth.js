fb9daac9ac6f1fe6f12d9a95f5de5c6d5bcb8d5d
Dalia Adel <daliaadel262@gmail.com>
2025-12-20 15:07:07 +0200

    fixed invalid token issue

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,13 +6,17 @@ const authenticate = async (req, res, next) => {
   try {
     let token;
 
-    if (req.headers.authorization) {
-      token = req.headers.authorization;
-    }
-    //or get token from cookie (browser)
-    else if (req.cookies && req.cookies.jwt) {
+    
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer ")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    } 
+    else if (req.cookies?.jwt) {
       token = req.cookies.jwt;
     }
+    
     if (!token) {
       return res.status(401).json({
         status: "fail",
