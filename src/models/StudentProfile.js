c7c983daced0580a221c852af7921f26a30ec1d9
Dalia Adel <daliaadel262@gmail.com>
2026-03-09 16:31:03 +0200

    added email in student profile model

--- src/models/StudentProfile.js
+++ src/models/StudentProfile.js
@@ -87,6 +87,12 @@ const studentProfileSchema = new Schema(
       required: true,
       trim: true,
     },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
 
     gpa: {
       type: Number,
